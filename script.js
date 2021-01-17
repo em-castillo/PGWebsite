79bb825d9c5d86fc3f0ae466f4f7a51a7bfe6cda
em-castillo <76987606+em-castillo@users.noreply.github.com>
2021-01-16 20:36:07 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let date = new Date();
-let year =  date.getFullYear();
-document.getElementById("actualYear").textContent = year;
-
-document.getElementById("lastUpdated").textContent = "Last Updated: " + document.lastModified;
