3aba155f25a0dbad243f5f81eaeca06bfba3b7d0
Emily <cas20037@byui.edu>
2021-02-13 22:16:29 -0700

    lesson6

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let year =  date.getFullYear();
 document.getElementById("actualYear").textContent = year;
 
 document.getElementById("lastUpdated").textContent = "Last Updated: " + document.lastModified;
+
+//font
+WebFont.load({
+    google: {
+      families: ["Playfair Display"],
+    },
+  });
