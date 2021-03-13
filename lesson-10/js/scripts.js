4652aaa03f360e1730758a4786cef097d6b4ed10
Emily <cas20037@byui.edu>
2021-03-12 23:30:20 -0700

    preston api

---
+++ lesson-10/js/scripts.js
@@ -0,0 +1,86 @@
+//WEATHER SUMMARY
+//ADD the key and change units to imperial
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=05f41fc7ad683844ae8d5bfe48afac6a&units=imperial";
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+.then((response) => response.json())
+.then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+    
+    document.getElementById('current').innerHTML= weatherInfo.weather[0].description; //check console for name
+    document.getElementById('temp').innerHTML= weatherInfo.main.temp_max;
+    document.getElementById('humid').innerHTML= weatherInfo.main.humidity;
+    document.getElementById('speed').innerHTML= weatherInfo.wind.speed;
+
+    //wind chill
+    const tempNumber =  weatherInfo.main.temp;
+    const speedNumber = weatherInfo.wind.speed;
+
+    let windchill = 35.75 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+    windchill =  Math.round(windchill);
+
+    if(tempNumber <= 50 && speedNumber > 3){
+      document.getElementById('chill').textContent = "Wind Chill: " +windchill+ "\xB0F";
+}
+    else{
+      document.getElementById('chill').textContent =  "No Wind Chill Today";
+
+} 
+});
+    
+//FIVE DAY FORECAST
+const d = new Date();
+
+const todayDayNumber = d.getDay();
+
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+const apiaURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=05f41fc7ad683844ae8d5bfe48afac6a&units=imperial";
+
+fetch(apiaURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    console.log(weatherInfo);
+    
+    let mylist = weatherInfo.list;
+      let forecastDayNumber = todayDayNumber;
+
+      for (i = 0; i < mylist.length; i++){
+
+        let time = mylist[i].dt_txt;
+        if (time.includes('18:00:00')) {
+
+          forecastDayNumber += 1;
+          if (forecastDayNumber === 7){forecastDayNumber = 0;}
+
+          let theDayName = document.createElement('h4');
+          theDayName.textContent = weekday[forecastDayNumber];
+
+          let theTemp = document.createElement('p');
+          theTemp.textContent = weatherInfo.list[i].main.temp + "\xB0";
+
+          let iconCode = weatherInfo.list[i].weather[0].icon;
+          let iconPath = '//openweathermap.org/img/w/' + iconCode + '.png';
+          let theIcon = document.createElement('img');
+          theIcon.src = iconPath;
+        
+          let theDay = document.createElement('div');
+          theDay.appendChild(theDayName);
+          theDay.appendChild(theTemp);
+          theDay.appendChild(theIcon);
+
+          document.getElementById('fiveDays').appendChild(theDay);
+  
+      }
+    }
+
+ }); //end of "then" fat arrow function
