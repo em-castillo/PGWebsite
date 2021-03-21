7dd88f9408ed35be3e45098543411e724ed67c07
Emily <cas20037@byui.edu>
2021-03-20 22:03:11 -0600

    icon

--- lesson-10/js/scripts.js
+++ lesson-10/js/scripts.js
@@ -70,7 +70,7 @@ fetch(apiURL)
           let iconCode = weatherInfo.list[i].weather[0].icon;
           let iconPath = '//openweathermap.org/img/w/' + iconCode + '.png';
           let theIcon = document.createElement('img');
-          theIcon.setAttribute("alt", weatherInfolist[0].weather[0].description)
+          theIcon.alt = `Icon image of ${weatherInfo.list[i].weather[0].description}`;
           theIcon.src = iconPath;
         
           let theDay = document.createElement('div');
