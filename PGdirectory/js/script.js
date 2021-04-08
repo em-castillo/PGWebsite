a54c61e6d8005d5e2acb74c4227a24c6fa0ec5ed
Emily <cas20037@byui.edu>
2021-04-07 23:14:13 -0600

    updated js

--- PGdirectory/js/script.js
+++ PGdirectory/js/script.js
@@ -37,11 +37,6 @@ const today = new Date();
       // console.log(dayNumber);
 
 
-let date = new Date(document.lastModified);
-let year =  date.getFullYear();
-document.getElementById("actualYear").textContent = year;
-      
-document.getElementById("lastUpdated").textContent = "Last Updated: " + document.lastModified;
 
 //font
 WebFont.load({
